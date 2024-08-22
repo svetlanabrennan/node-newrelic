f69e15e44629e398131147c2e976d33f9aab5661
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2024-08-22 15:47:43 -0500

    remove nr name

--- cloud-tooling/azure-site-extension/Content/install.cmd
+++ cloud-tooling/azure-site-extension/Content/install.cmd
@@ -1,6 +1,3 @@
-:: Copyright 2024 New Relic Corporation. All rights reserved.
-:: SPDX-License-Identifier: Apache-2.0
-
 @echo off
 REM Call the PowerShell script
 powershell.exe -ExecutionPolicy Bypass -File .\install.ps1
--- cloud-tooling/azure-site-extension/Content/uninstall.cmd
+++ cloud-tooling/azure-site-extension/Content/uninstall.cmd
@@ -1,6 +1,3 @@
-:: Copyright 2024 New Relic Corporation. All rights reserved.
-:: SPDX-License-Identifier: Apache-2.0
-
 SET NEW_RELIC_FOLDER="%HOME%\node_modules/newrelic"
 IF EXIST %NEW_RELIC_FOLDER% (
   npm uninstall newrelic
